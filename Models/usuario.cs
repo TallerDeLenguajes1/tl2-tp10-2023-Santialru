43ae2cb6ad174653d3def83c7f8d2bc77b2f6f61
Santiago <alvarezrubiosanti@gmail.com>
2023-12-01 13:45:43 -0300

    haciendo viewmodel usuario

--- Models/usuario.cs
+++ Models/usuario.cs
@@ -1,7 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using tp11.ViewModels;
+
 namespace tp9.Models;
+public enum NivelDeAcceso
+{
+    admin = 1,
+    operador = 2
+}
 public class Usuario
 {
-    public int Id{get;set;}
-    public string NombreDeUsuario{get;set;}
+    public NivelDeAcceso NivelDeAcceso {get;set;}
+    public int Id {get;set;}
+    public string NombreDeUsuario {get;set;}
+
+    public Usuario(LoginViewModel LoginViewModel)
+    {
+        NombreDeUsuario = LoginViewModel.NombreDeUsuario;
+        Contrasenia = LoginViewModel.Contrasenia;
+    }
+
+    public Usuario()
+    {
+    }
 
 }
